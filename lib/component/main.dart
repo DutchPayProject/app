138b5af47224426e5bb79752edda652172266184
ByeonKwon <cbk5126@gmail.com>
2024-12-12 15:17:04 +0900

    feat/main 기능 구현

--- lib/component/main.dart
+++ lib/component/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import '../group.dart';
 
 void main() {
-  runApp(const DutchMain());
+  runApp(const MaterialApp(
+    home: DutchMain(),
+      debugShowCheckedModeBanner: false,
+  ));
 }
-
 class DutchMain extends StatelessWidget {
   const DutchMain({super.key});
 
@@ -13,6 +16,7 @@ class DutchMain extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: GestureDetector(
         onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => DutchGroup(),));
         },
         child: Scaffold(
           body: Center(
@@ -22,7 +26,7 @@ class DutchMain extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Image.asset('Doller.png', height: 300, width: 200,),
+                  Image.asset('Doller.png', height: 200, width: 100,),
                   Text('Dutch Pay', style: TextStyle(color: Color(0xff22844c), fontSize: 40,fontFamily:'Jua'),),
                   Text('# 더치페이의 선택 1등 앱', style: TextStyle(color: Color(0xff22844c), fontSize: 30,fontFamily:'Jua'),),
                   Text('화면을 터치해주세요', style: TextStyle(color: Color(0xff22844c), fontSize: 30, fontFamily:'Jua'),)
@@ -35,6 +39,7 @@ class DutchMain extends StatelessWidget {
     );
   }
 }
+
 
 
 
