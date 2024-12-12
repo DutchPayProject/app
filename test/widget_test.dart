dae2c45febd46f655118989fa5f664081ba202ed
ByeonKwon <cbk5126@gmail.com>
2024-12-12 15:18:35 +0900

    feat/test.dart 내부 MyApp -> DutchMain 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dutch_pay/main.dart';
+import 'package:dutch_pay/component/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
